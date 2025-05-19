f36b7affce5b64d8505cebbac72d8d465bfdc489
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-19 21:29:14 +0530

    Continued

--- BruteForcePassword/bruteforcepassword.py
+++ BruteForcePassword/bruteforcepassword.py
@@ -5,3 +5,10 @@ file_path=str(input('Enter path to the file to bruteforce with: '))
 hash_to_decrypt=str(input('Enter Hash Value to Bruteforce: '))
 
 with open(file_path, 'r') as file:
+    for line in file.readlines():
+        if type_of_hash=='md5':
+            hash_object=hashlib.md5(line.strip().encode())
+            hash_word==hash_object.hexdigest()
+            if hash_word==hash_to_decrypt:
+                print('Found MD5 Password: ' + line.strip())
+                exit (0)
