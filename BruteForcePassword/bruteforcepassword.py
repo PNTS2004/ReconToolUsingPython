c6f56806c9d0f1a37b765330c26a73cc538dd769
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-20 22:40:32 +0530

    Progess Continued

--- BruteForcePassword/bruteforcepassword.py
+++ BruteForcePassword/bruteforcepassword.py
@@ -8,7 +8,14 @@ with open(file_path, 'r') as file:
     for line in file.readlines():
         if type_of_hash=='md5':
             hash_object=hashlib.md5(line.strip().encode())
-            hash_word==hash_object.hexdigest()
-            if hash_word==hash_to_decrypt:
+            hashed_word==hash_object.hexdigest()
+            if hashed_word==hash_to_decrypt:
                 print('Found MD5 Password: ' + line.strip())
+                exit (0)
+
+        if type_of_hash=='sha1':
+            hash_object=hashlib.sha1(line.strip().encode())
+            hashed_word==hash_object.hexdigest()
+            if hashed_word==hash_to_decrypt:
+                print('Found SHA1 Password: ' + line.strip())
                 exit (0)
